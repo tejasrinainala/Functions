e5e83940034a373b01bbf829c1ca200e92e00434
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-06-07 17:17:46 +0530

    Create challenge2.js

---
+++ challenge2.js
@@ -0,0 +1,18 @@
+// Challenge: Write a countdown function with a hard-coded starting number inside closure
+// Stretch goal: Write a countdown function that can count from a provided number,
+// with a provided step
+
+// Start
+function countdown(startingNumber, step) {
+  let countFromNum = startingNumber;
+  return function decrease() {
+    countFromNum -= step;
+    return countFromNum;
+  }
+}
+
+const countingDown = countdown(11, 1);
+
+console.log(countingDown());
+console.log(countingDown());
+console.log(countingDown());
